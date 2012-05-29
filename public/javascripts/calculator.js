83c341338ff3361acf33fa8fb4c093a46802b9a1
Jonas Forsberg <himynameisjonas@gmail.com>
2012-05-29 14:07:33 +0200

    reload if application cache is old

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,3 +1,20 @@
+window.addEventListener('load', function(e) {
+  if (window.applicationCache) {
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+          // Browser downloaded a new app cache.
+          // Swap it in and reload the page to get the new hotness.
+          window.applicationCache.swapCache();
+          if (confirm('A new version of this site is available. Load it?')) {
+            window.location.reload();
+          }
+        } else {
+          // Manifest didn't changed. Nothing new to server.
+        }
+    }, false);
+  }
+}, false);
+
 function calculateChargesFromAmount(amount){
     var charges = 1.5;
     charges += amount * 0.0275;
